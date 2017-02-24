d98f99ef0646435ba13e91141de86b5652ce79a2
Michael Pearce <themikepearce@gmail.com>
2017-02-23 20:20:01 -0800

    slight modification of file

--- app.py
+++ app.py
@@ -6,13 +6,34 @@ from flask import Flask
 # create the application object
 app = Flask(__name__)
 
-# use decorators to link the funciton to a url
+# use decorators to link the function to a url
 @app.route("/")
 @app.route("/hello")
 
 # define the view using a function, which returns a string
 def hello_world():
 	return "Hello, World!"
+
+#dynamic route
+@app.route("/test/<search_query>")
+def search(search_query):
+	return search_query
+
+@app.route("/integer/<int:value>")
+def int_type(value):
+	print value + 1
+	return "correct"
+
+@app.route("/float/<float:value>")
+def float_type(value):
+	print value + 1.0
+	return "CORRECT"
+
+#dynamic route that accepts slashes
+@app.route("/path/<path:value>")
+def path_type(value):
+	print value
+	return "THEES EES CORRRRRECT"
 
 # start the development server using the run() method
 if __name__ == "__main__":
